d133339c4913e2ddd5849770276d313f6421eaf3
Pierre <pierresavelli22@gmail.com>
2025-06-19 13:58:24 -0300

    Commit

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print("Hello, World!")
+run = True
+menu = True
+play = False
+rules = False
+
+while run:
+    if menu:
+        print("Welcome to the game!")
+        print("1. Play")
+        print("2. Rules")
+        print("3. Exit")
+        choice = input("Please choose an option: ")
+
+        if choice == '1':
+            menu = False
+            play = True
+        elif choice == '2':
+            menu = False
+            rules = True
+        elif choice == '3':
+            run = False
+        else:
+            print("Invalid choice, please try again.")
+
+    elif play:
+        print("Starting the game...")
+        # Game logic goes here
+        play = False  # Reset to go back to menu
+
+    elif rules:
+        print("Game Rules:")
+        print("1. Rule one")
+        print("2. Rule two")
+        input("Press Enter to return to the menu...")
+        rules = False  # Reset to go back to menu
